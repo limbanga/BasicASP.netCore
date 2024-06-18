2442711246a72ebe58697b5d81720cd750c74067
limbanga <95570833+limbanga@users.noreply.github.com>
2024-06-18 20:26:38 +0700

    Add project files.

---
+++ Data/ClothesStoreContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ClothesStore.Models;
+
+namespace ClothesStore.Data
+{
+    public class ClothesStoreContext : DbContext
+    {
+        public ClothesStoreContext (DbContextOptions<ClothesStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ClothesStore.Models.Product> Product { get; set; } = default!;
+    }
+
+}
---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ClothesStore.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; } = 0;
+    }
+}
